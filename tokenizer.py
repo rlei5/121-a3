e560991617f398f0a2c45fad5ad7fd139c3e70fd
13Characters_ <benjt0523@gmail.com>
2026-05-15 19:27:35 -0700

    Finish get_raw_text and extract_important_words

--- tokenizer.py
+++ tokenizer.py
@@ -3,10 +3,22 @@ from bs4 import BeautifulSoup
 
 def get_raw_text(html_content: str):
     """Given the HTML source of a webpage in html_content, this function returns the raw text from that webpage."""
+    raw_text = []
     soup = BeautifulSoup(html_content)
+    for tag in soup.descendants:
+        if tag.string:
+            raw_text.append(tag.string)
+    return " ".join(raw_text)
+
 def extract_important_words(html_content: str):
     """Given the HTML source of a webpage, return the important words on that page."""
-    pass
+    soup = BeautifulSoup(html_content)
+    important_words = tokenize(soup.title) # The title always contains important words, according to the specification
+    for tag in soup.descendants:
+        if tag.string and tag.name in ['b', 'h1', 'h2', 'h3']: # Anything in bold text, h1, h2, h3 tags is also considered important
+            important_words.extend(tokenize(tag.string))
+    return important_words
+
 def tokenize(text: str) -> list[str]:
     """Tokenize the text. The search engine specification defines a token as an alphanumeric sequence."""
     tokens = []
